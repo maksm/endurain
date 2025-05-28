0ba113a88af08a7842d03d4e641465358cfac08b
Maks Mržek <maks.mrzek@gmail.com>
2025-05-28 16:50:48 +0200

    add summaries page to mobile navigation

--- frontend/app/src/config/navigation.js
+++ frontend/app/src/config/navigation.js
@@ -1,6 +1,7 @@
 export const mobileNavItems = [
   { routeName: 'home', icon: ['fas', 'fa-home'], labelKey: 'navbarBottomMobileComponent.home' },
   { routeName: 'activities', icon: ['fas', 'fa-person-running'], labelKey: 'navbarBottomMobileComponent.activities' },
+  { routeName: 'summary', icon: ['fas', 'fa-calendar-alt'], labelKey: 'navbarBottomMobileComponent.summary' },
   { routeName: 'gears', icon: ['fas', 'fa-bicycle'], labelKey: 'navbarBottomMobileComponent.gear' },
   { routeName: 'health', icon: ['fas', 'fa-heart'], labelKey: 'navbarBottomMobileComponent.health' },
   { routeName: 'menu', icon: ['fas', 'bars'], labelKey: 'navbarBottomMobileComponent.menu' },
